dc08528755aacd81cc49cc2db78bea64e4e440d3
Maria <marikoreneva@gmail.com>
2018-04-11 18:01:46 +0300

    Changed submodules

--- analyzer.py
+++
@@ -1,8 +0,0 @@
-from BeTrip_Images.ImageAnalytics import ImageAnalytics
-from BeTrip_Images.image_analysis.image_helper import open_img
-
-
-ia = ImageAnalytics()
-im = open_img("../../instagram/1/1/1.jpg")
-score = ia.get_score(im)
-print(score)
